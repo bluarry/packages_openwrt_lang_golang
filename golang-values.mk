c5f140f145b12707a7482212c836df0cd1ae02a0
bluarry <1002587829@qq.com>
2025-08-21 21:56:16 +0800

    Update golang-values.mk

--- golang-values.mk
+++ golang-values.mk
@@ -196,7 +196,7 @@ endif
 
 # Target Go
 
-GO_ARCH_DEPENDS:=@(aarch64||arm||i386||i686||mips||mips64||mips64el||mipsel||powerpc64||x86_64)
+GO_ARCH_DEPENDS:=@(aarch64||arm||arm64||i386||i686||mips||mips64||mips64el||mipsel||powerpc64||x86_64)
 
 
 # ASLR/PIE
